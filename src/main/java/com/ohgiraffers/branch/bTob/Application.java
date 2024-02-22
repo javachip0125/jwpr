c0792e2ac57a9fa7b75be1edcd8023ad05350576
byulyang <ckdlghd3791@naver.com>
2024-02-23 06:53:20 +0900

    first commit

---
+++ src/main/java/com/ohgiraffers/branch/bTob/Application.java
@@ -0,0 +1,20 @@
+package com.ohgiraffers.branch.bTob;
+
+import java.util.Scanner;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Application ap = new Application();
+
+    }
+
+    public int inputNum() {
+
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        return num;
+        /* master 생성 후 등록*/
+    }
+}
